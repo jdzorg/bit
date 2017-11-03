8e1562f18f21f74b6f0dd1da1781abfd998ab2dd
jdzorg <web.voloshyn@gmail.com>
2017-11-03 18:19:57 +0200

    initial commit

---
+++ src/pages/index/app.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+
+    export default {
+        data () {
+            return {
+                ruleForm: {
+                    phone: '',
+                    password: ''
+                },
+                rules: {}
+            }
+        }
+    }
+</script>
+
+<style src="../../assets/css/index.sass" lang="sass"></style>
